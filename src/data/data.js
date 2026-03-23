d9f73ead5efd56f2c89182e0f68b8209badea3bb
Guilherme Gomes <guilhermegomes040806@gmail.com>
2026-03-23 14:12:02 -0300

    criação array de objetos

---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+const shinobiDexData = [
+    {id: 1, nome: "Naruto",categoria: "personagem",descrição: "Ninja da aldeia da folha"},
+    {id: 2, nome: "Sasuke",categoria: "personagem",descrição: "Ninja da aldeia da folha e vingador Uchiha"},
+    {id: 3, nome: "Sakura",categoria: "personagem",descrição: "Ninja da aldeia da folha e cabelo rosa"},
+    {id: 4, nome: "Kakashi",categoria: "personagem",descrição: "Ninja da aldeia da folha e sensei do time 7"},
+    {id: 5, nome: "Gaara",categoria: "personagem",descrição: "Ninja da aldeia da areia e kazekage"},
+    {id: 6, nome: "Kunai",categoria: "arma",descrição: "lamina ninja padrão"},
+    {id: 7, nome: "Shuriken",categoria: "arma",descrição: "Arma de lançamento usada por ninjas"},
+    {id: 8, nome: "Konohagakure",categoria: "Aldeia",descrição: "Aldeia ninja do país do fogo"},
+    {id: 9, nome: "Sharingan",categoria: "kekke-genkai",descrição: "kekke-genkai dos Uchiha"},
+    {id: 10, nome: "Rasengan",categoria: "jutsu",descrição: "jutsu criado pelo Minato e usado pelo Naruto"},
+]
+
